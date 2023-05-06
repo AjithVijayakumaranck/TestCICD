19ac6b9767b615c3f751fc476ff51374c4be1c8a
ajithvijayakumaran <aji001ajith@gmail.com>
2023-05-06 17:21:07 +0530

    ORder

--- Backend/Controllers/checkoutControllers.js
+++ Backend/Controllers/checkoutControllers.js
@@ -3,6 +3,26 @@ module.exports = {
 
     //stripe checkout
     sripeCheckout : (req,res)=>{
-        
+        app.post('/create-checkout-session', async (req, res) => {
+            const session = await stripe.checkout.sessions.create({
+              line_items: [
+                {
+                  price_data: {
+                    currency: 'usd',
+                    product_data: {
+                      name: 'T-shirt',
+                    },
+                    unit_amount: 2000,
+                  },
+                  quantity: 1,
+                },
+              ],
+              mode: 'payment',
+              success_url: 'http://localhost:4242/success',
+              cancel_url: 'http://localhost:4242/cancel',
+            });
+          
+            res.redirect(303, session.url);
+          });
     }
 }
