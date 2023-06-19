c5b86364ac7ac094153c4500f9cfd19159feff29
Ajith Vijayakmaran <aji001ajith@gmail.com>
2023-06-19 12:14:33 +0530

    review done

--- Backend/Controllers/profile_controller.js
+++ Backend/Controllers/profile_controller.js
@@ -8,21 +8,21 @@ const { verifyEmail } = require("./user_registration");
 
 module.exports = {
 
-    //get profile function
-    getProfile: async (req,res)=>{
+     //get profile function
+     getProfile: async (req, res) => {
         try {
-            const {userId} = req.params
-            const profileDetails = await USER.findOne({_id:userId})
-            if(profileDetails){
-                 res.status(200).json(profileDetails)
-                }else{
-                 res.status(400).json({message:"user not found"})
+            const { userId } = req.params
+            const profileDetails = await USER.findOne({ _id: userId })
+            if (profileDetails) {
+                res.status(200).json(profileDetails)
+            } else {
+                res.status(400).json({ message: "user not found" })
             }
         } catch (error) {
-            res.status(500).json({messgae:"something went wrong"})
+            res.status(500).json({ messgae: "something went wrong" })
         }
     },
-    
+
 
     //profile updateFunction
     updateProfile: async (req, res) => {
@@ -71,163 +71,174 @@ module.exports = {
             res.status(500).json({ messgae: "something went wrong" })
         }
     },
-   
+
 
     //email update
-    updateEmail: async (req,res)=>{
+    updateEmail: async (req, res) => {
         try {
-            const {userId,currentEmail,updatingEmail} = req.body
-            const profileDetails = await  USER.findOne({_id:userId,googleVerified:false})
+            console.log(req.body, "update profile");
+            const { userId, currentEmail, updatingEmail } = req.body
+            const profileDetails = await USER.findOne({ _id: userId, googleVerified: false })
+            console.log(updatingEmail, "update emails profile det");
             // const profileDetails = await  USER.findOne({_id:userId})
-            if(!profileDetails ){
-                res.status(404).json({message:"user not found"})
-            }else{
-                const emailUsed = await USER.findOne({email:updatingEmail,googleVerified:false})
-                if(emailUsed){
-                    res.status(500).json({message:"Email already used"})
-                }else{
-                    const createdOTP = await sendOTP(updatingEmail);
-                    res.status(200).json({message:"OTP sented "})
+            if (!profileDetails) {
+                res.status(404).json({ message: "user not found" })
+            } else {
+                const emailUsed = await USER.findOne({ email: updatingEmail, googleVerified: false })
+                console.log(emailUsed, "email used");
+                if (emailUsed) {
+                    res.status(500).json({ message: "Email already used" })
+                } else {
+                    console.log(updatingEmail, "update emails22");
+                    const createdOTP = await sendOTP({ email: updatingEmail });
+                    res.status(200).json({ message: "OTP sented " })
                 }
             }
         } catch (error) {
-            res.status(500).json({messsage:"something went wrong"})
+            console.log(error, "500 error");
+            res.status(500).json({ messsage: "something went wrong" })
         }
     },
-    
+
     //update email verification
-    updateEmailVerification : async (req,res)=>{
-      try {
-        const {otp ,updatingEmail,userId} = req.body
-        const profileDetails = await USER.findOne({_id:userId,googleVerified:false})
-        if(!profileDetails){
-           res.status(404).json({message:"user not found"})
-        }else{
-            const otpInfo = await OTP.findOne({email:updatingEmail});
-            if(!otpInfo){
-               res.status(400).json({message:"OTP is invalid"})
-            }else{
-                const verified = await verifyHashedData(otp, otpInfo.otp)
-                if(!verified){
-                   res.status(400).json({message:"invalid OTP"})
-                }else{
-                  USER.updateOne({_id:profileDetails._id},{
-                    $set : {
-                        email:updatingEmail
+    updateEmailVerification: async (req, res) => {
+        try {
+            const { otp, updatingEmail, userId } = req.body
+            const profileDetails = await USER.findOne({ _id: userId, googleVerified: false })
+            if (!profileDetails) {
+                res.status(404).json({ message: "user not found" })
+            } else {
+                const otpInfo = await OTP.findOne({ email: updatingEmail });
+                if (!otpInfo) {
+                    res.status(400).json({ message: "OTP is invalid" })
+                } else {
+                    const verified = await verifyHashedData(otp, otpInfo.otp)
+                    if (!verified) {
+                        res.status(400).json({ message: "invalid OTP" })
+                    } else {
+                        USER.updateOne({ _id: profileDetails._id }, {
+                            $set: {
+                                email: updatingEmail
+                            }
+                        }, { upsert: true }).then(() => {
+                            res.status(200).json({ message: "email updated successfully" })
+                        }).catch(() => {
+                            res.status(400).json({ message: "email updating failed" })
+                        })
                     }
-                  },{upsert:true}).then(()=>{
-                    res.status(200).json({message:"email updated successfully"})
-                }).catch(()=>{
-                    res.status(400).json({message:"email updating failed"})
-                  })
                 }
+
             }
-            
+        } catch (error) {
+            res.status(500).json({ messsage: "something went wrong" })
         }
-      } catch (error) {
-        res.status(500).json({messsage:"something went wrong"})
-      }
     },
 
 
     //add or update new phonenumber
-    addPhoneNumber : async (req,res)=>{
+    addPhoneNumber: async (req, res) => {
         try {
-            const {phonenumber,userId} = req.body
-
-            const userDetails = await USER.findOne({_id:userid})
-
-            if(!userDetails){
-                  res.status(404).json({message:"user not found"})
-            }else{
-                sentVerificationOtp(phonenumber).then(()=>{
-                        res.status(200).json({message:"OTP sented successfully"})
-                }).catch(()=>{
-                        res.status(400).json({message:"OTP not sented"})
+            console.log(req.body, "updating phone");
+            const { phonenumber, userId } = req.body
+            const userDetails = await USER.findOne({ _id: userId })
+            if (!userDetails) {
+                res.status(404).json({ message: "user not found" })
+            } else {
+                sentVerificationOtp(phonenumber).then(() => {
+                    res.status(200).json({ message: "OTP sented successfully" })
+                }).catch(() => {
+                    res.status(400).json({ message: "OTP not sented" })
                 })
             }
 
         } catch (error) {
-            res.status(500).json({message:"something went wrong"})
+            res.status(500).json({ message: "something went wrong" })
         }
     },
 
 
     //verfication and updating phonenumber
-    addPhoneNumberVerification : async (req,res)=>{
+    addPhoneNumberVerification: async (req, res) => {
         try {
-            const {phoneNumber,otp,userId} = req.body
-            verifyPhoneOtp(phoneNumber, otp).then(async() => {
-                console.log("otp approved",phonenumber);
-                await  USER.updateOne({$and : [{ _id:userId},{googleVerified:false}]},{ phoneNumber:phoneNumber},{upsert:true});
+            console.log(req.body, "updating phone verification");
+            const { phoneNumber, otp, userId } = req.body
+            verifyPhoneOtp(phoneNumber, otp).then(async () => {
+                console.log("otp approved", phoneNumber);
+                await USER.updateOne({ $and: [{ _id: userId }, { googleVerified: false }] }, { phoneNumber: phoneNumber }, { upsert: true });
                 res.status(200).json({ message: "Phonenumber Updated" });
-            }).catch(() => {
+            }).catch((error) => {
+                console.log(error);
                 res.status(400).json({ message: "invalid otp" });
             })
         } catch (error) {
-            res.status(500).json({message:"something went wrong"})
+            res.status(500).json({ message: "something went wrong" })
         }
     },
 
 
     //update password
-    updatePassword :async (req,res) => {
+    updatePassword: async (req, res) => {
+        console.log(req.body,"update  password");
         try {
-            const {currentPassword,newPassword,userId} = req.body 
+            const { currentPassword, newPassword, userId } = req.body
 
-            const userDetails = await USER.findOne({_id:userId})
+            const userDetails = await USER.findOne({ _id: userId })
 
-            if(!userDetails) {
-                    res.status(404).json({message:"User not found"})
-            }else{
-              const verified = await  verifyHashedData(currentPassword,userDetails.password) 
-              if(!verified){
-                        res.status(400).json({message:"Password is incorrect"})
-              }else{
-                const HashedPassword =await hashData(newPassword)
-                USER.updateOne({_id:userDetails._id},{password:HashedPassword}).then(()=>{
-                        res.status(200).json({messagge:"password updated"})
-                    }).catch(()=>{
-                        res.status(400).json({messagge:"password not updated"})
+            if (!userDetails) {
+                res.status(404).json({ message: "User not found" })
+            } else {
+                console.log(userDetails," user details");
+                const verified = await verifyHashedData(currentPassword, userDetails.password)
+
+                if (!verified) {
+                    res.status(400).json({ message: "Password is incorrect" })
+                } else {
+                    const HashedPassword = await hashData(newPassword)
+                    console.log(HashedPassword,"sdfdfgf")
+                    USER.updateOne({ _id: userDetails._id }, { password: HashedPassword }).then(() => {
+                        res.status(200).json({ messagge: "password updated" })
+                    }).catch((err) => {
+                        console.log(err);
+                        res.status(400).json({ messagge: "password not updated" })
                     })
-              }
+                }
             }
         } catch (error) {
-            res.status(500).json({message:"something went wrong"})
+            res.status(500).json({ message: "something went wrong" })
         }
     },
 
     //softdeleting profile
-    deleteProfile: async (req,res)=>{
+    deleteProfile: async (req, res) => {
         try {
-            const {Id} = req.params
-            const userDetails = await USER.findOne({_id:Id})
-            if(!userDetails){
-                    res.status(404).json({message:"user not found"})
-            }else{
-                    USER.updateOne({_id:Id},{deleted:true}).then(()=>{
-                        res.status(200).json({message:"User details"})
-                    }).catch(()=>{
-                        res.status(400).json({message:"Failed to delete"})
-                    })
+            const { Id } = req.params
+            const userDetails = await USER.findOne({ _id: Id })
+            if (!userDetails) {
+                res.status(404).json({ message: "user not found" })
+            } else {
+                USER.updateOne({ _id: Id }, { deleted: true }).then(() => {
+                    res.status(200).json({ message: "User details" })
+                }).catch(() => {
+                    res.status(400).json({ message: "Failed to delete" })
+                })
             }
 
         } catch (error) {
-            res.status(500).json({message:"something went wrong"})
+            res.status(500).json({ message: "something went wrong" })
         }
     },
-    
-    getAllProfiles :async (req,res)=>{
+
+    getAllProfiles: async (req, res) => {
         try {
-             const allProfiles = await USER.find({deleted:false})
-             if(!allProfiles){
-                 console.log(err);
-                 res.status(404).json({message:""})
-                }else{
-                    res.status(200).json(allProfiles)
-                }
+            const allProfiles = await USER.find({ deleted: false })
+            if (!allProfiles) {
+                console.log(err);
+                res.status(404).json({ message: "" })
+            } else {
+                res.status(200).json(allProfiles)
+            }
         } catch (error) {
-            res.status(500).json({message:"something went Wrong"})
+            res.status(500).json({ message: "something went Wrong" })
         }
     },
 
@@ -236,32 +247,38 @@ module.exports = {
     profileRating: async (req,res)=>{
         try {
             console.log("Iam here");
-            const {userId = "646ca82602c3b676a3224f24",ratingId ="6481c467a5d1d3f751d79175",star ="5",comment = "Hello not bad"} = req.body
+            const {userId,ratingId,star,comment} = req.body
             const userDetails =await USER.findOne({_id:userId})
 
             if(!userDetails){  
                 console.log("responseee");   
                 res.status(404).json({message:"user Not found"})
             }else{
-                console.log("ressss",userDetails);
-
+                console.log("ressss");
                 let alreadyRated = await userDetails.ratings.find(
-                    (userId) => userId.postedby.toString() === _id.toString()
+                    (userId) => userId.postedby.toString() === ratingId.toString()
                   )
 
                   if(alreadyRated){
-                     console.log("already rated");
+                     console.log(alreadyRated,"already rated");
                       const updateRating = await USER.updateOne(
                           {
                             ratings: { $elemMatch: alreadyRated },
                           },
                           {
-                            $set: { "ratings.$.star": star, "ratings.$.comment": comment },
-                          },
+                            $set: { 
+                                // ratings: {
+                                //     star: star,
+                                //     comment: comment,
+                                //   }
+                                // },
+                                "ratings.$.star": star, "ratings.$.comment": comment },
+                            },
                           {
                             new: true,
                           }
                         );
+                        console.log(updateRating,"hellosss");
                   }else{
                     console.log("not rated rated");
                     const rateProfile = await USER.findByIdAndUpdate(
@@ -282,25 +299,26 @@ module.exports = {
                   }
                 
             }
-            const getallratings = await USER.findById(prodId);
+            const getallratings = await USER.findById(userId);
             let totalRating = getallratings.ratings.length;
             let ratingsum = getallratings.ratings
               .map((item) => item.star)
               .reduce((prev, curr) => prev + curr, 0);
             let actualRating = Math.round(ratingsum / totalRating);
             USER.findByIdAndUpdate(
-              prodId,
+              userId,
               {
                 totalrating: actualRating,
               },
               { new: true }
             ).then((response)=>{
-                console.log(response,"hello response");
-                res.status(200).json(finalUser);
+                res.status(200).json(response);
             }).catch((err)=>{
+                console.log(err.message);
                 res.status(400).json(err)
             })
         } catch (error) {
+            console.log(error.message);
             res.status(400).json(error)
         }
     },
@@ -308,7 +326,7 @@ module.exports = {
     //reply rating
     ratingReplay :async (req,res)=>{
         try {
-            const {reviewerId="646ca82602c3b676a3224f24",senderId = "6481c467a5d1d3f751d79175",reply="hekllo buuddd"} = req.body
+            const {reviewerId,senderId,reply} = req.body
             const userProfile =await USER.findById(reviewerId)
             if(!userProfile){
                 
@@ -327,7 +345,7 @@ module.exports = {
                 }else{
                     const replyData = {
                         content: reply,
-                        repliedBy: senderId // Replace with the actual user ID
+                        repliedBy: senderId
                       };
                     const updateRating = await USER.updateOne(
                         {
@@ -352,12 +370,12 @@ module.exports = {
              res.status(500).json({error})
         }
     },
+
     getReviews :async (req,res)=>{
         try {
-            console.log("iam here");
-            const {userId,page} = req.params
+        
+            const {userId,page} = req.query
             const limit = 10
-
             const rating = await USER.findById(userId).populate('ratings.reply.repliedBy').skip(page).limit(limit)
             console.log(rating,"hello");
             res.json(rating)
--- Backend/Models/favouriteModel.js
+++ Backend/Models/favouriteModel.js
@@ -2,13 +2,18 @@ const mongoose= require('mongoose');
 const schema = mongoose.Schema;
 
 const WISHLISTschema = new schema({
+
     userId:{
         type:String,
         unique:true,
         require:true
     },
-   wishlist:[String]
+   wishlist:[{
 
+    type:String,
+    ref:"products"
+    
+   }]
  
 },{timestamps:true})
 const WISHLIST =  mongoose.model("wishlist",WISHLISTschema);
