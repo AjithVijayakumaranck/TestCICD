a6eadccbea3c78ea9d9bf8ddff8cd32045e41181
anudin0899 <anudhinkk@gmail.com>
2024-01-23 16:27:36 +0530

    add phone number in contact form

--- frontend/src/Components/ContactForm/ContactForm.jsx
+++ frontend/src/Components/ContactForm/ContactForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Style from "./index.module.css"
 import { MdOutlineLocationOn, MdOutlineMail } from 'react-icons/md';
 import { FiClock } from 'react-icons/fi';
+import { BsTelephone } from 'react-icons/bs';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 import instance from '../../instance/AxiosInstance';
 import { toast } from 'react-toastify';
@@ -53,6 +54,10 @@ const ContactForm = () => {
                                         <li>
                                             <i><MdOutlineMail /></i>
                                             <span>contact.in@dealnbuy.co.in</span>
+                                        </li>
+                                        <li>
+                                            <i><BsTelephone /></i>
+                                            <span>+91 7907658092</span>
                                         </li>
                                     </ul>
                                 </div>
