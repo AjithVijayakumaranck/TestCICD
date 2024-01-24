c532384cd5076cfa129839f3f26f31a3ac2d43db
Ajith Vijayakumaran <aji001ajith@gmail.com>
2024-01-24 12:41:58 +0530

    otp

--- Backend/utilities/sendmail.js
+++ Backend/utilities/sendmail.js
@@ -2,24 +2,27 @@ const nodemailer = require('nodemailer')
 
 const { AUTH_EMAIL, AUTH_PASS } = process.env
 
+
 //creating transporter
 let transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com",
     auth: {
         user: AUTH_EMAIL,
         pass: AUTH_PASS
+
     },
     tls: {
         rejectUnauthorized: false
     }
-
+    
 })
 
 //test transporter
 transporter.verify((error, success) => {
     if (error) {
-        console.log(error.message,"hello");
-       throw error
+        // console.log(AUTH_EMAIL,AUTH_PASS);
+        console.log(error.message);
+        throw error
     } else {
         console.log("transporter working fine");
     }
