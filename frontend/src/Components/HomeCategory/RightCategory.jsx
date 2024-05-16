1239b32aedc2f3aa5cc6cf97312323d215da4d8a
arjun <arjunarunachalan@gmail.com>
2024-05-16 12:50:51 +0530

    popuar category

--- frontend/src/Components/HomeCategory/RightCategory.jsx
+++ frontend/src/Components/HomeCategory/RightCategory.jsx
@@ -1,41 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Style from "./Style.module.css";
-import { useState } from "react";
 import instance from "../../instance/AxiosInstance";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import authInstance from "../../instance/AuthInstance";
 import { UserContext } from "../../Contexts/UserContext";
 import registerClicks from "../../utilities/getCategoryClicks";
 
 const RightCategory = () => {
   const navigate = useNavigate();
   const userData = useContext(UserContext);
-  const { User, SetUser } = userData;
+  const { User } = userData;
   const userId = User?._id;
-
-  console.log("userid : ", userId);
 
   const [Categories, SetCategories] = useState([]);
   const [DisplayLimit, SetDisplayLimit] = useState(8);
 
-  const loadcategory = () => {
+  const loadCategories = () => {
     instance
       .get("/api/category/get_categories")
       .then((response) => {
-        SetCategories([...response.data]);
+        response.data.map((singlecategory) => {
+        console.log("unsorted",singlecategory?.clicks.length);
+          SetCategories(sortedArray);
+          console.log(sortedClicks.length, "srted clicks");
+        });
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  //LoadCategory functions
   useEffect(() => {
-    loadcategory();
+    loadCategories();
   }, []);
 
-  // popular category
   const onClickFun = (catId, userId) => {
     registerClicks(catId, userId);
     navigate(`/category/${catId}`);
@@ -48,19 +45,17 @@ const RightCategory = () => {
           <h3>Explore Popular Category</h3>
         </div>
         <div className={Style.box_wrapper}>
-          {Categories.slice(0, DisplayLimit).map((category, index) => {
-            return (
-              <div className={Style.item} key={index}>
-                <div
-                  className={Style.box}
-                  onClick={() => onClickFun(category?._id)}
-                >
-                  <img src={category?.icon?.url} alt="" />
-                </div>
-                <h6>{category?.categoryName}</h6>
+          {Categories.slice(0, DisplayLimit).map((category, index) => (
+            <div className={Style.item} key={index}>
+              <div
+                className={Style.box}
+                onClick={() => onClickFun(category?._id, userId)}
+              >
+                <img src={category?.icon?.url} alt="" />
               </div>
-            );
-          })}
+              <h6>{category?.categoryName}</h6>
+            </div>
+          ))}
         </div>
       </div>
     </>
